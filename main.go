2766d7b9a2cd63ff5470a899cca1e6d326ee6a9d
Kecy <keishiro.oym@gmail.com>
2017-09-24 07:44:22 +0900

    message elapse time changed: 1h -> 90min

--- main.go
+++ main.go
@@ -170,7 +170,7 @@ func deleteOldMessages(api *slack.Client, q chan string){
 */
 
 func deleteTimer(api *slack.Client, timestamp string) {
-  time.Sleep(1.5 * time.Hour)
+  time.Sleep(90 * time.Minute)
   respChannel, respTimestamp, err := api.DeleteMessage(slackChannelID, timestamp)
   if err != nil {
     fmt.Printf("Slack API Error(%v): %v\n", timestamp, err)
